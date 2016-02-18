c873efa652316980907ee1248a88e1fe82ae1456
Ryan LaBouve <imryanlabouve@gmail.com>
2016-02-17 22:47:42 -0600

    some top bar action

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,6 @@
 import ActiveModelAdapter from 'active-model-adapter';
 import config from '../config/environment';
 
-
 export default ActiveModelAdapter.extend({
   authorizer: 'authorizer:devise',
   namespace: config.API.namespace,
