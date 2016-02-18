9e8e67788dddeca888c501e9f91fabbdad476730
Ryan LaBouve <imryanlabouve@gmail.com>
2016-02-17 22:59:20 -0600

    I haz messed up authentication

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,8 @@
 import ActiveModelAdapter from 'active-model-adapter';
 import config from '../config/environment';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default ActiveModelAdapter.extend({
+export default ActiveModelAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:devise',
   namespace: config.API.namespace,
   host: config.API.host
--- app/models/user.js
+++ app/models/user.js
@@ -4,4 +4,6 @@ export default DS.Model.extend({
   username: DS.attr('string'),
   email: DS.attr('string'),
   gravatarUrl: DS.attr('string'),
+  password: DS.attr('string'),
+  passwordConfirmation: DS.attr('string')
 });
